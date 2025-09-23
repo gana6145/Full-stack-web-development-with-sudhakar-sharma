3f27be7cccb35f85f2b8084ff42db83f162775b2
Ganesh Jadhav <jadhav.ganesh1700@gmail.com>
2025-09-23 11:05:51 +0530

    started learning js

---
+++ JavaScript practise folder/day1/app.js
@@ -0,0 +1,17 @@
+console.log("hello ganesh to javaSript");
+let a = 10;
+let b = 20;
+// console.log("sum of a and b will be ", a + b);
+// console.log(`sum of youe ${a} and ${b} will be ${a+b}`);
+
+// let penPrice;
+// penPrice=prompt('Enter the pen price');
+// console.log(`penprice is the ${penPrice}`);
+
+let c=10;
+let d=2;
+
+a+=d;
+console.log(a);
+
+// console.log(c**d);//10*10
